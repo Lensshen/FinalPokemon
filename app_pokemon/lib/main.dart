db44417cd2443b5e66d6c6f7260bb253f6f411b5
Leans <Leans_ortega@unitec.edu>
2025-06-05 22:12:17 -0600

    Google autentication funcional a

--- app_pokemon/lib/main.dart
+++ app_pokemon/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:flutter/material.dart';
 import 'IniciarSecion.dart';
 
 void main() async {
@@ -10,7 +10,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
