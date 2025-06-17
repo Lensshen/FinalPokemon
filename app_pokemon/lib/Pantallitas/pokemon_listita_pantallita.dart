d866a6ac81b7a1d9d558f1350daeded140a74262
oscar-javier <oscarjav99@unitec.edu>
2025-06-17 00:22:36 -0600

    cambio de tamaño en lista pokemon

--- app_pokemon/lib/Pantallitas/pokemon_listita_pantallita.dart
+++ app_pokemon/lib/Pantallitas/pokemon_listita_pantallita.dart
@@ -38,10 +38,10 @@ class _PokemonListitaPantallitaState extends State<PokemonListitaPantallita> {
             return GridView.builder(
               padding: const EdgeInsets.all(12),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, // Ahora se muestran 3 cartas por fila
+                crossAxisCount: 3, 
                 crossAxisSpacing: 12,
                 mainAxisSpacing: 12,
-                childAspectRatio: 0.85, // Puedes ajustar esto para afinar la altura
+                childAspectRatio: 1.50, 
               ),
               itemCount: lista.length,
               itemBuilder: (context, index) {
